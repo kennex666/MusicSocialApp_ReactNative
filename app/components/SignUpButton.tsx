b36d79963a3940a585ac6b95d058b37227e865de
Zennisch <snowy.sunny.dazing.us@gmail.com>
2024-11-09 16:37:51 +0900

    add: SignUpButton.tsx

---
+++ app/components/SignUpButton.tsx
@@ -0,0 +1,23 @@
+import { Text, TouchableOpacity } from "react-native";
+import { vw } from "../utils/ViewpointEmulator";
+import { COLORS } from "../constants/color";
+import { FontSizeConstants } from "../constants/font-size";
+
+export default function SignUpButton(props: any): JSX.Element {
+    return (
+        <TouchableOpacity style={[{
+            width: vw(80),
+            height: vw(10),
+            backgroundColor: COLORS.spotify.background,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 100,
+        }]}>
+            <Text style={[{
+                color: COLORS.spotify.text,
+                fontSize: FontSizeConstants.nm,
+                fontWeight: 'bold',
+            }]}>Sign up free</Text>
+        </TouchableOpacity>
+    )
+}
