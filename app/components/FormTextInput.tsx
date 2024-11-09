8d9e676a9e0aa4a3797cf1c00b6695a0f7e01fbb
Zennisch <snowy.sunny.dazing.us@gmail.com>
2024-11-09 20:24:19 +0900

    update: FormTextInput font size

--- app/components/FormTextInput.tsx
+++ app/components/FormTextInput.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, TextInput } from "react-native";
 import { vh, vw } from "../utils/ViewpointEmulator";
 import { COLORS } from "../constants/color";
+import { FontSizeConstants } from "../constants/font-size";
 
 export default function FormTextInput(props: any): JSX.Element {
     return <TextInput style={styles.textInput} />;
@@ -14,5 +15,6 @@ const styles = StyleSheet.create({
         backgroundColor: COLORS.tertiary.background,
         paddingHorizontal: 20,
         borderRadius: 5,
+        fontSize: FontSizeConstants.nm,
     },
 });
