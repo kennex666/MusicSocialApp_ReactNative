da726d13ab0d950a34dda7a6f59a1205e5e96546
Zennisch <snowy.sunny.dazing.us@gmail.com>
2024-11-09 16:40:48 +0900

    update: SignUpButton.tsx

--- app/components/SignUpButton.tsx
+++ app/components/SignUpButton.tsx
@@ -4,20 +4,32 @@ import { COLORS } from "../constants/color";
 import { FontSizeConstants } from "../constants/font-size";
 
 export default function SignUpButton(props: any): JSX.Element {
+    const { onPress } = props;
     return (
-        <TouchableOpacity style={[{
-            width: vw(80),
-            height: vw(10),
-            backgroundColor: COLORS.spotify.background,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: 100,
-        }]}>
-            <Text style={[{
-                color: COLORS.spotify.text,
-                fontSize: FontSizeConstants.nm,
-                fontWeight: 'bold',
-            }]}>Sign up free</Text>
+        <TouchableOpacity
+            style={[
+                {
+                    width: vw(80),
+                    height: vw(10),
+                    backgroundColor: COLORS.spotify.background,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: 100,
+                },
+            ]}
+            onPress={onPress}
+        >
+            <Text
+                style={[
+                    {
+                        color: COLORS.spotify.text,
+                        fontSize: FontSizeConstants.nm,
+                        fontWeight: "bold",
+                    },
+                ]}
+            >
+                Sign up free
+            </Text>
         </TouchableOpacity>
-    )
+    );
 }
