e043a9d43ff01382ad848b9ce72870e32f6b12b8
Zennisch <snowy.sunny.dazing.us@gmail.com>
2024-11-09 19:19:29 +0900

    update: SignUpButton.tsx style

--- app/components/SignUpButton.tsx
+++ app/components/SignUpButton.tsx
@@ -1,35 +1,29 @@
-import { Text, TouchableOpacity } from "react-native";
-import { vh, vw } from "../utils/ViewpointEmulator";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import { COLORS } from "../constants/color";
 import { FontSizeConstants } from "../constants/font-size";
+import { vh, vw } from "../utils/ViewpointEmulator";
 
 export default function SignUpButton(props: any): JSX.Element {
     const { onPress } = props;
     return (
-        <TouchableOpacity
-            style={[
-                {
-                    width: vw(80),
-                    height: vh(6),
-                    backgroundColor: COLORS.spotify.background,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: 100,
-                },
-            ]}
-            onPress={onPress}
-        >
-            <Text
-                style={[
-                    {
-                        color: COLORS.spotify.text,
-                        fontSize: FontSizeConstants.nm,
-                        fontWeight: "bold",
-                    },
-                ]}
-            >
-                Sign up free
-            </Text>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+            <Text style={styles.text}>Sign up free</Text>
         </TouchableOpacity>
     );
 }
+
+const styles = StyleSheet.create({
+    button: {
+        width: vw(80),
+        height: vh(5.3),
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 100,
+        backgroundColor: COLORS.spotify.background,
+    },
+    text: {
+        color: COLORS.spotify.text,
+        fontSize: FontSizeConstants.nm,
+        fontWeight: "bold",
+    },
+});
