649adbbb38d80ad9c601d85d87a0a599a284499f
Dương Thái Bảo <me@dtbao.io.vn>
2024-11-10 18:17:06 +0700

    add: PlayerControl, ImageButton & image const

---
+++ app/constants/image_resource.ts
@@ -0,0 +1,12 @@
+export const IMAGE_RESOURCE = {
+  player: {
+    repeat: require("../assets/player/icon.png"),
+    repeatActive: require("../assets/player/icon.png"),
+    shuffle: require("../assets/player/icon.png"),
+    shuffleActive: require("../assets/player/icon.png"),
+    skipBack: require("../assets/player/icon.png"),
+    skipForward: require("../assets/player/icon.png"),
+    play: require("../assets/player/icon.png"),
+    pause: require("../assets/player/icon.png"),
+  },
+};
